918c6a47e2e8807c3a1251324cda7d1c29d470fc
JieeHD <jordan.craig94@gmail.com>
2012-05-17 22:27:37 +0100

    Doh, never run as root

--- pullfiles.sh
+++ pullfiles.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 #VillainMod Git Handler
 
-# Run as root, of course.
+echo "Welcome to the VillainMod Git Handler"
 
-ROOT_UID=0     # Only users with $UID 0 have root privileges.
-LINES=50       # Default number of lines saved.
-E_XCD=86       # Can't change directory?
-E_NOTROOT=87   # Non-root exit error.
-
-if [ "$UID" -ne "$ROOT_UID" ]
-then
-  echo "Must be root to run this script."
-  exit $E_NOTROOT
-else
-  echo "Welcome to the VillainMod Git Handler"
-fi  
 
 if [ -n "$1" ]
 # Test whether command-line argument is present (non-empty).
